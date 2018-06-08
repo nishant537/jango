dacd6c238680aa8462aec0ebc0197847391c9079
Vishnu Tadimeti <vishnutadimeti@purdue.edu>
2018-06-08 03:24:08 -0700

    Added new UI

--- app.py
+++ app.py
@@ -1,40 +1,105 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from functools import wraps
 from werkzeug.utils import secure_filename
-import os
-import sys
+import requests
 import json
-import time
-import signal
-import config
-
-# TODO: Import files and replace mock data being passed into the templates
-
-# import camera_controller
-# import licensing
-# import performance
-# from utils import StoppableThread, bgcolors, set_yolo_flag, get_yolo_flag
-# from camera_database import CameraDatabase
-# from auto_notifier import start_monitor_thread, stop_monitor_thread
 
 # Initiate Flask
 app = Flask(__name__)
+
+license_key_valid = False
 
 # If needed
 # UPLOAD_FOLDER = '/path/to/license'
 # app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Handle HTTP Requests
+
+############# ALL GET
+
+# @app.route('/getLicense', methods=['GET', 'POST'])
+# def url1():
+#     if request.method == 'GET':
+#         data = requests.get('127.0.0.1:8081/getLicense').content
+#     return data
+
+# @app.route('/getAllCameraInfo', methods=['GET', 'POST'])
+# def url2():
+#     if request.method == 'GET':
+#         data = requests.get('127.0.0.1:8081/getAllCameraInfo').content
+#     return data
+
+# @app.route('/getCameraInfo/', methods=['GET', 'POST'])
+# def url3():
+#     if request.method == 'GET':
+#         data = requests.get('127.0.0.1:8081/getCameraInfo/').content
+#     return data
+# # Join these two, somehow
+# @app.route('/alertInfo', methods=['GET', 'POST'])
+# def url4():
+#     if request.method == 'GET':
+#         data = requests.get('127.0.0.1:8081/alertInfo').content
+#     return data
+
+# @app.route('/getBackground', methods=['GET', 'POST'])
+# def url5():
+#     if request.method == 'GET':
+#         data = requests.get('127.0.0.1:8081/getBackground').content
+#     return data
+
+
+############# ALL POSTS
+
+# @app.route('/licenseUpdate', methods=['GET', 'POST'])
+# def url6():
+#     return data
+
+# @app.route('/createCamera', methods=['GET', 'POST'])
+# def url7():
+#     return data
+
+# @app.route('/editCamera/', methods=['GET', 'POST'])
+# def url8():
+#     return data
+
+# # Join these two?
+
+# @app.route('/deleteCamera/', methods=['GET', 'POST'])
+# def url9():
+#     return data
+
+# @app.route('/sendBackground', methods=['GET', 'POST'])
+# def url10():
+#     return data
+
+# Decorator - Checking for license first before loading any page
+def license_required(func):
+    @wraps(func)
+    def license_validity(*args, **kwargs):
+        if license_key_valid is False:
+            return redirect(url_for('landing'))
+        return func(*args, **kwargs)
+    return license_validity
+
 # Flask Routing
 @app.route('/')
-@app.route('/home')
 def landing():
-
-    # Initialize
+    # TODO: Initialize function - Get all the data from Backend first
 
     # TODO: Check for license validation - Backend
+    return render_template('landing.html')
 
+@app.route('/home')
+def home():
     return render_template('home.html')
 
-@app.route('/license', methods=['GET', 'POST'])
+@app.route('/list')
+def list_view():
+    return render_template('list.html')
+
+# Data Handling from GUI
+
+@app.route('/licenseUpload', methods=['GET', 'POST'])
 def license():
     # Upload license
     if request.method == 'POST':
@@ -42,40 +107,19 @@ def license():
 
         # Create a filename of the file uploaded (Using original file name of the license)
         filename = secure_filename(f.filename)
-        print 'The filename of the image uploaded is: ' + filename
+        print 'The filename of the license uploaded is: ' + filename
 
         # Save file to the directory, if needed
         # f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         
-        # Change license message after validation
-        license_message = 'Updating...'
-        
     # TODO: Check license validity - Backend
-    return render_template('home.html', message = license_message)
-
+    return render_template('home.html')
+  
 ################## TODO: If valid license, then route the following
 
-@app.route('/add')
-def add():
-
-    # TODO: Need to load data from Database - Backend
-    
-    return render_template('add.html')
-
-@app.route('/view')
-def view():
-
-    # TODO: Retrieve camera information - Backend
-
-    return render_template('view.html')
-    
-@app.route('/registeration')
-def registeration():
-    return render_template('registeration.html')
-
 @app.route('/data', methods=['GET', 'POST'])
+@license_required
 def data():
-
     # Data retrieved from Registeration form
     camera_name = request.form['cameraname']
     floor = request.form['floor']
