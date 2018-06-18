c09b4a74f66da6dad2c55cdabbd7417f42c4389d
Vishnu Tadimeti <vishnutadimeti@purdue.edu>
2018-06-17 21:01:47 -0700

    Remove Modals - Separate Pages for Add and Edit

--- app.py
+++ app.py
@@ -16,18 +16,18 @@ def get_license():
     license_validity = license_payload['validity']
     return license_status, license_validity
 
+# Get Camera info from backend
+def get_camera_info():
+    all_camera_info = requests.get('http://127.0.0.1:8081/getAllCameraInfo').content
+    camera_info = json.loads(all_camera_info)
+    return camera_info
+    
 # Get Background info from backend
 def get_background():
     get_background = requests.get('http://127.0.0.1:8081/getBackground').content
     background_payload = json.loads(get_background)
     background_img = background_payload['image']
     return background_img
-
-# Get Camera info from backend
-def get_camera_info():
-    all_camera_info = requests.get('http://127.0.0.1:8081/getAllCameraInfo').content
-    camera_info = json.loads(all_camera_info)
-    return camera_info
 
 # Decorator - Checking for license first before loading any page
 def license_required(func):
@@ -53,10 +53,61 @@ def landing():
         license_message = 'Invalid. Please upload a valid license'
     return render_template('landing.html', message = license_message, validity = license_validity, image = img)
 
-# Route home
+# Route Add Camera page
+@app.route('/add')
+@license_required
+def add_camera_page():
+    img = get_background()
+    return render_template('add.html', image = img)
+
+# Route Edit Camera page
+@app.route('/edit/<camera_id>')
+@license_required
+def edit_camera_page(camera_id):
+    img = get_background()
+    camera_payload = get_camera_info()
+    current_email = []
+    current_sms = []
+    current_call = []
+
+    for i in range(0, len(camera_payload)):
+
+        if camera_id == camera_payload[str(i)]['camera_id']:
+
+            for j in range(0, len(camera_payload[str(i)]['email_list'])):
+                current_email.append(str(camera_payload[str(i)]['email_list'][j]))
+                current_email = ''.join(current_email)
+
+            for k in range(0, len(camera_payload[str(i)]['sms_list'])):
+                current_sms.append(str(camera_payload[str(i)]['sms_list'][k]))
+                current_sms = ''.join(current_sms)
+
+            for l in range(0, len(camera_payload[str(i)]['call_list'])):
+                current_call.append(str(camera_payload[str(i)]['call_list'][l]))
+                current_call = ''.join(current_call)
+
+            current_name = camera_payload[str(i)]['camera_name']
+            current_floor = camera_payload[str(i)]['floor']
+            current_start_time = camera_payload[str(i)]['intrusion_start_time']
+            current_end_time = camera_payload[str(i)]['intrusion_end_time']
+            current_rtsp = camera_payload[str(i)]['rtsp_url']
+            current_http = camera_payload[str(i)]['http_url']
+
+    return render_template('edit.html', image = img, 
+    current_name = current_name, 
+    current_floor = current_floor,
+    current_rtsp = current_rtsp,
+    current_http = current_http,
+    current_email = current_email,
+    current_sms = current_sms,
+    current_call = current_call,
+    current_start_time = current_start_time,
+    current_end_time = current_end_time)
+
+# Route home page
 @app.route('/home')
 @license_required
-def home():
+def home_page():
     img = get_background()
     camera_payload = get_camera_info()
     camera_names_list = []
@@ -86,14 +137,15 @@ def home():
     
     return render_template('home.html', image = img, camera = camera_names_list, favourites = favourites_list, unique_floors = unique_floors, camera_floor = cameras_in_floor_dict)
 
-# Route list
+# Route list page
 @app.route('/list')
 @license_required
-def list_view():
+def list_page():
     img = get_background()
     camera_payload = get_camera_info()
 
     camera_names_list = []
+    camera_id_list = []
     floors_list = []
     favourites_list = []
     start_time_list = []
@@ -123,6 +175,7 @@ def list_view():
             call_list.append(str(camera_payload[str(i)]['call_list'][l]))
 
         camera_names_list.append(str(camera_payload[str(i)]['camera_name']))
+        camera_id_list.append(str(camera_payload[str(i)]['camera_id']))
         floors_list.append(str(camera_payload[str(i)]['floor']))
         favourites_list.append(str(camera_payload[str(i)]['favourite']))
         start_time_list.append(str(camera_payload[str(i)]['intrusion_start_time']))
@@ -138,6 +191,7 @@ def list_view():
 
     return render_template('list.html', image = img,
     data = zip(
+        camera_id_list,
         camera_names_list,
         rtsp_url_list,
         email_list,
@@ -177,7 +231,7 @@ def background_image(background_image):
     background_dict["image"] = img
     post_background_info = requests.post(url = 'http://127.0.0.1:8081/sendBackground', data = background_dict)
 
-    return redirect(url_for('home'))
+    return redirect(url_for('home_page'))
     
 # Handle license upload 
 @app.route('/licenseUpload', methods=['GET', 'POST'])
@@ -188,17 +242,15 @@ def license():
         f = request.files['license_file']
         # Using original file name
         filename = secure_filename(f.filename)
-        print 'The filename of the license uploaded is: ' + filename     
-        #TODO: Run license script
-    return redirect(url_for('home'))
+        print 'The filename of the license uploaded is: ' + filename
+    return redirect(url_for('home_page'))
 
-# Add Camera - Get form info
+# Add Camera
 @app.route('/addCamera', methods=['GET', 'POST'])
 @license_required
 def add_camera():
-    print 'Adding Camera'
-    #TODO: Switch to Flask-WTF (Form and Data Handling)
 
+    # Get new camera data from form
     if request.method == 'POST':
         name = request.form['camera_name']
         floor = request.form['floor']
@@ -213,34 +265,85 @@ def add_camera():
         sound_alarm = request.form.getlist('sound_alarm')
         object_detection = request.form.getlist('object_detection')
 
-        print 'Camera Name: ' + name
-        print 'Floor: ' + floor
-        print 'Main Stream URL: ' + main_url
-        print 'Email List: ' + email
-        print 'Sub Stream URL: ' + sub_url
-        print 'SMS List: ' + sms
-        print 'Start Time: ' + start_time
-        print 'Call List: ' + call
-        print 'End Time: ' + end_time
-        print 'Favourite: ' + str(favourite)
-        print 'Objects: ' + str(object_detection)
+        print 'Adding Camera'
+        print 'New Camera Name: ' + name
+        print 'New Floor: ' + floor
+        print 'New Main Stream URL: ' + main_url
+        print 'New Email List: ' + email
+        print 'New Sub Stream URL: ' + sub_url
+        print 'New SMS List: ' + sms
+        print 'New Start Time: ' + start_time
+        print 'New Call List: ' + call
+        print 'New End Time: ' + end_time
+        print 'New Favourite: ' + str(favourite)
+        print 'New Objects: ' + str(object_detection)
 
-        camera_dict = OrderedDict()
-        camera_dict["camera_name"] = name
-        camera_dict["email_list"] = email
-        camera_dict["sms_list"] = sms
-        camera_dict["call_list"] = call
-        camera_dict["rtsp_url"] = main_url
-        camera_dict["http_url"] = sub_url
-        camera_dict["floor"] = floor
-        camera_dict["object_detect"] = object_detection
-        camera_dict["intrusion_start_time"] = start_time
-        camera_dict["intrusion_end_time"] = end_time
-        camera_dict["sound_alarm"] = sound_alarm
+        new_camera_dict = OrderedDict()
+        new_camera_dict["camera_name"] = name
+        new_camera_dict["email_list"] = email
+        new_camera_dict["sms_list"] = sms
+        new_camera_dict["call_list"] = call
+        new_camera_dict["rtsp_url"] = main_url
+        new_camera_dict["http_url"] = sub_url
+        new_camera_dict["floor"] = floor
+        new_camera_dict["object_detect"] = object_detection
+        new_camera_dict["intrusion_start_time"] = start_time
+        new_camera_dict["intrusion_end_time"] = end_time
+        new_camera_dict["sound_alarm"] = sound_alarm
 
         # Making a POST to the Backend - New Camera
-        post_new_camera_info = requests.post(url = 'http://127.0.0.1:8081/createCamera', data = camera_dict)
-    return redirect(url_for('home'))
+        post_new_camera_info = requests.post(url = 'http://127.0.0.1:8081/createCamera', data = new_camera_dict)
+    return redirect(url_for('home_page'))
+
+# Edit Camera
+@app.route('/editCamera', methods=['GET', 'POST'])
+@license_required
+def edit_camera():
+
+    # Get edited camera data from form
+    if request.method == 'POST':
+        name = request.form['camera_name']
+        floor = request.form['floor']
+        main_url = request.form['main_stream_url']
+        email = request.form['email_id_list']
+        sub_url = request.form['sub_stream_url']
+        sms = request.form['sms_list']
+        call = request.form['call_list']
+        start_time = request.form['intrusion_start_time']
+        end_time = request.form['intrusion_end_time']
+        favourite = request.form.getlist('favourite')
+        sound_alarm = request.form.getlist('sound_alarm')
+        object_detection = request.form.getlist('object_detection')
+
+        print 'Editing Camera'
+        print 'Edited Camera Name: ' + name
+        print 'Edited Floor: ' + floor
+        print 'Edited Main Stream URL: ' + main_url
+        print 'Edited Email List: ' + email
+        print 'Edited Sub Stream URL: ' + sub_url
+        print 'Edited SMS List: ' + sms
+        print 'Edited Start Time: ' + start_time
+        print 'Edited Call List: ' + call
+        print 'Edited End Time: ' + end_time
+        print 'Edited Favourite: ' + str(favourite)
+        print 'Edited Objects: ' + str(object_detection)
+
+        edited_camera_dict = OrderedDict()
+        edited_camera_dict["camera_name"] = name
+        edited_camera_dict["email_list"] = email
+        edited_camera_dict["sms_list"] = sms
+        edited_camera_dict["call_list"] = call
+        edited_camera_dict["rtsp_url"] = main_url
+        edited_camera_dict["http_url"] = sub_url
+        edited_camera_dict["floor"] = floor
+        edited_camera_dict["object_detect"] = object_detection
+        edited_camera_dict["intrusion_start_time"] = start_time
+        edited_camera_dict["intrusion_end_time"] = end_time
+        edited_camera_dict["sound_alarm"] = sound_alarm
+
+        # Making a POST to the Backend - Edited Camera
+        post_edited_camera_info = requests.post(url = 'http://127.0.0.1:8081/createCamera', data = edited_camera_dict)
+    return redirect(url_for('list_page'))
 
 if __name__ == "__main__":
     # Running Flask
